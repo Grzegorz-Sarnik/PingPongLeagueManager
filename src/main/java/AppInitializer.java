b8efe093c4d1062e400a3c6613ee91985cf0c821
Grzegorz_Sarnik <sarnik.g@gmail.com>
2023-08-26 17:07:40 +0200

    configuration

---
+++ src/main/java/AppInitializer.java
@@ -0,0 +1,33 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class AppInitializer implements WebApplicationInitializer {
+    public void onStartup(ServletContext container) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx =
+                new AnnotationConfigWebApplicationContext();
+        ctx.register(AppConfig.class);
+
+        ctx.setServletContext(container);
+        ServletRegistration.Dynamic servlet =
+                container.addServlet("dispatcher", new DispatcherServlet(ctx));
+
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+    }
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return filter;
+}}
